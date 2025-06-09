1926fd389c904c6ec8f5bf33f161b3ed5371faae
Dominik Adrian Grzywak <starterx4@gmail.com>
2025-06-10 01:37:15 +0200

    up

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
  BINDIR=$(dirname "$(readlink -fn "$0")")
  cd "$BINDIR"
-/lib/jvm/java-24-jdk/bin/java -XX:-UseAdaptiveSizePolicy -Xmn128M -XX:+UseNUMA -XX:MaxTenuringThreshold=15 -XX:MaxGCPauseMillis=30 -XX:GCPauseIntervalMillis=150 -XX:-UseGCOverheadLimit -XX:SurvivorRatio=8 -XX:TargetSurvivorRatio=90 -XX:MaxTenuringThreshold=15 -Dfml.ignorePatchDiscrepancies=true -Dfml.ignoreInvalidMinecraftCertificates=true -XX:+UseCompressedOops -XX:+OptimizeStringConcat -XX:ReservedCodeCacheSize=2048m -XX:+UseCodeCacheFlushing -XX:SoftRefLRUPolicyMSPerMB=10000 -XX:ParallelGCThreads=4 -DIReallyKnowWhatIAmDoingISwear -jar minecraft_server.jar 
+screen -d -m -S mc /lib/jvm/zulu-21-amd64/bin/java -XX:-UseAdaptiveSizePolicy -Xmn128M -XX:+UseNUMA -XX:MaxTenuringThreshold=15 -XX:MaxGCPauseMillis=30 -XX:GCPauseIntervalMillis=150 -XX:-UseGCOverheadLimit -XX:SurvivorRatio=8 -XX:TargetSurvivorRatio=90 -XX:MaxTenuringThreshold=15 -Dfml.ignorePatchDiscrepancies=true -Dfml.ignoreInvalidMinecraftCertificates=true -XX:+UseCompressedOops -XX:+OptimizeStringConcat -XX:ReservedCodeCacheSize=2048m -XX:+UseCodeCacheFlushing -XX:SoftRefLRUPolicyMSPerMB=10000 -XX:ParallelGCThreads=4 -DIReallyKnowWhatIAmDoingISwear -jar minecraft_server.jar 
 #--forceUpgrade
